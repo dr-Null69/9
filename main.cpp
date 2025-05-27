f7e4af386aa1f7e27e76d9c1169937ce8ee04af2
dr-Null69 <naguranskajvitalij@gmail.com>
2025-05-27 23:23:30 +0300

    зробленно перше завдання

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+
+int main() {
+    std::vector<std::thread> threads;
+
+    for (int i = 1; i <= 3; ++i) {
+        threads.emplace_back([i]() {
+            int result = i * i;
+            std::cout << "Thread " << i << ": " << i << "^2 = " << result << "\n";
+        });
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    return 0;
+}
